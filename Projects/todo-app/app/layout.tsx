a9116b6c7731de497137aa562b06649a67e6372f
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-12 14:20:47 +0500

    Add a submit button

--- Projects/todo-app/app/layout.tsx
+++ Projects/todo-app/app/layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <h1 style={{textAlign:'center'}}>Todo App</h1>
+        {children}
+        </body>
     </html>
   )
 }
--- Projects/todo-app/app/page.tsx
+++ Projects/todo-app/app/page.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+
+let todos=async function(){
+  let res=await fetch('http://localhost:3001/api/todo/list')
+  let data=await res.json()
+  console.log('AAAAAAAAAAAAAAAAAAaa',data.todos)
+  return data.todos
+}
+todos()
+
 export default function Page() {
   return (
-    <div>Hello world!</div>
+    <div>
+      <div>
+      <input type="text" />
+      <button type='submit'>Add</button>
+    </div>
+    </div>
   )
 }

42c489689187cafec26e3e45a607ca8645623fb7
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-27 23:25:23 +0500

    Complete structure of pricing for mobile

--- Projects/pricing-ui/app/components/card.tsx
+++ Projects/pricing-ui/app/components/card.tsx
@@ -1,4 +1,9 @@
 "use client";
+import Circle from "../assets/mdCheckedCircle/Circle.png";
+import FirstIcon from "../assets/FirstIcon.png";
+import SecondIcon from "../assets/SecondIcon.png";
+import ThirdIcon from "../assets/ThirdIcon.png";
+import Image from "next/image";
 import {
   Button,
   ButtonGroup,
@@ -7,40 +12,90 @@ import {
   Divider,
   Heading,
   Text,
-  Image,
   Stack,
   Box,
   Card,
   VStack,
   CardHeader,
+  List,
+  ListItem,
+  ListIcon,
 } from "@chakra-ui/react";
 
 export default function MyCard() {
   return (
     <>
-      <Box>
-        <Box bg={"#6B46C1"} h={"394px"} w={"428px"} padding={10}>
-          <Stack spacing={'5'}>
-            <Heading color={"#F7FAFC"} fontSize="2xl" fontWeight={"800"}>
-              Simple pricing for your business
-            </Heading>
-            <Text color={"#F7FAFC"}>
-              Plans that are carefully crafted to suit your business.
-            </Text>
-            <Card bg={'#F0EAFB'} p={'8'}>
-              <CardBody textAlign={'center'}>
-               <Stack spacing={'3'}>
-                <Heading fontSize={'24px'}>Premium PRO</Heading>
-                <Text fontSize={'5xl'} fontWeight={'800'}>$329</Text>
-                <Text>billed just once</Text>
-                <Button variant="solid" bg="#805AD5" color={'#F7FAFC'} px={'75px'} _hover={{color:'#805AD5',bg:'white'}} >
-                Get Started
-                </Button>
+      <Box bg={"#6B46C1"} h={"394px"} w={"428px"} padding={10}>
+        <Stack spacing={"5"}>
+          <Heading color={"#F7FAFC"} fontSize="2xl" fontWeight={"800"}>
+            Simple pricing for your business
+          </Heading>
+          <Text color={"#F7FAFC"}>
+            Plans that are carefully crafted to suit your business.
+          </Text>
+          <Card>
+            <CardBody textAlign={"center"} p={"0"}>
+              <Box bg={"#F0EAFB"} p={"8"}>
+                <Stack spacing={"3"}>
+                  <Heading fontSize={"24px"}>Premium PRO</Heading>
+                  <Text fontSize={"5xl"} fontWeight={"800"}>
+                    $329
+                  </Text>
+                  <Text>billed just once</Text>
+                  <Button
+                    variant="solid"
+                    bg="#805AD5"
+                    color={"#F7FAFC"}
+                    px={"75px"}
+                    _hover={{ color: "#805AD5", bg: "white" }}
+                  >
+                    Get Started
+                  </Button>
                 </Stack>
-              </CardBody>
-            </Card>
-          </Stack>
-        </Box>
+              </Box>
+              <List spacing={5} pl={3} py={"40px"}>
+                <ListItem>
+                  Access these features when you get this pricing package for
+                  your business.
+                </ListItem>
+                <ListItem display={"flex"}>
+                  <Image src={Circle} alt="logo" />
+                  <Text pl={3}>International calling and messaging API</Text>
+                </ListItem>
+                <ListItem display={"flex"}>
+                  <Image src={Circle} alt="logo" />
+                  <Text pl={3}>Additional phone numbers</Text>
+                </ListItem>
+                <ListItem display={"flex"}>
+                  <Image src={Circle} alt="logo" />
+                  <Text pl={3}>Automated messages via Zapier</Text>
+                </ListItem>
+                <ListItem display={"flex"}>
+                  <Image src={Circle} alt="logo" />
+                  <Text pl={3}>24/7 support and consulting</Text>
+                </ListItem>
+              </List>
+            </CardBody>
+          </Card>
+        </Stack>
+        <List spacing={8} pl={1} py={"40px"}>
+          <ListItem display={"flex"}>
+            <Image src={FirstIcon} alt="logo" />
+            <Text as="b" pl={3}>
+              30 days money back Guarantree
+            </Text>
+          </ListItem>
+          <ListItem as="b" display={"flex"}>
+            <Image src={SecondIcon} alt="logo" />
+            <Text as="b" pl={3}>
+              No setup fees 100% hassle-free
+            </Text>
+          </ListItem>
+          <ListItem as="b" display={"flex"}>
+            <Image src={ThirdIcon} alt="logo" />
+            <Text pl={3}>No monthly subscription Pay once and for all</Text>
+          </ListItem>
+        </List>
       </Box>
     </>
   );
--- Projects/pricing-ui/app/sajid/page.tsx
+++ Projects/pricing-ui/app/sajid/page.tsx
@@ -1,24 +1,24 @@
-// async function getData() {
-//     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-//     // The return value is *not* serialized
-//     // You can return Date, Map, Set, etc.
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
   
-//     // Recommendation: handle errors
-//     if (!res.ok) {
-//       // This will activate the closest `error.js` Error Boundary
-//       throw new Error('Failed to fetch data');
-//     }
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
   
-//     return res.json();
-//   }
-// export default async function TestAgain() {
-//     const data = await getData();
-//     let arr=[...data]
-//     // console.log(arr)
-//     return <main>
-//         {arr.map(e=>{
-//           return <div>{e.title}</div>
-//         })}
+    return res.json();
+  }
+export default async function TestAgain() {
+    const data = await getData();
+    let arr=[...data]
+    // console.log(arr)
+    return <main>
+        {arr.map(e=>{
+          return <div>{e.title}</div>
+        })}
 
-//     </main>;
-//   }
+    </main>;
+  }

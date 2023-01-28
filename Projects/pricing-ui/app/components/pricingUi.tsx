c0db2678efdac0a51e670844c03f28cf3e58deb8
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-28 22:42:54 +0500

    Started the project again with responsive design.

---
+++ Projects/pricing-ui/app/components/pricingUi.tsx
@@ -0,0 +1,24 @@
+"use client";
+import {
+  Button,
+  ButtonGroup,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Text,
+  Stack,
+  Box,
+  Card,
+  VStack,
+  CardHeader,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react";
+
+export default function PricingUi() {
+  return (
+    <>This is pricingUi </>
+  );
+}
--- Projects/pricing-ui/app/page.tsx
+++ Projects/pricing-ui/app/page.tsx
@@ -1,16 +1,13 @@
 'use client';
-import { Inter } from '@next/font/google'
-import MyCard from './components/card'
-import { ChakraProvider } from '@chakra-ui/react'
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+import PricingUi from './components/pricingUi'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Home() {
   return (<>
-  <div className={inter.className}>
+  <div>
     <ChakraProvider>
-      <MyCard/>
+      <PricingUi/>
     </ChakraProvider>
   </div>
     </>

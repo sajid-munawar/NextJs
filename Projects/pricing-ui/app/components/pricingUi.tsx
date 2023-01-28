f38456707a76926b35993d2f07120b1e9e927026
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-28 23:09:22 +0500

    Remove useless imports

--- Projects/pricing-ui/app/components/pricingUi.tsx
+++ Projects/pricing-ui/app/components/pricingUi.tsx
@@ -1,30 +1,17 @@
 "use client";
-import {
-  Button,
-  ButtonGroup,
-  CardBody,
-  CardFooter,
-  Divider,
-  Heading,
-  Text,
-  Stack,
-  Box,
-  Card,
-  VStack,
-  CardHeader,
-  List,
-  ListItem,
-  ListIcon,
-  Container,
-} from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function PricingUi() {
   return (
     <>
-    <Box bg={'#6B46C1'} textAlign='center' color={'#F7FAFC'} h={'397px'}>
-      <Heading fontSize={'48px'} fontWeight={'800'} pt={'90px'}>Simple pricing for your business</Heading>
-      <Text fontSize={'24px'} fontWeight={'500'}>Plans that are carefully crafted to suit your business.</Text>
-    </Box>
+      <Box bg={"#6B46C1"} textAlign="center" color={"#F7FAFC"} h={"397px"}>
+        <Heading fontSize={"48px"} fontWeight={"800"} pt={"90px"}>
+          Simple pricing for your business
+        </Heading>
+        <Text fontSize={"24px"} fontWeight={"500"}>
+          Plans that are carefully crafted to suit your business.
+        </Text>
+      </Box>
     </>
   );
 }

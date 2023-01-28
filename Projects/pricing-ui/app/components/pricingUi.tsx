069f68bec27ad5adef4af1ecc738fe4b8b46b9b6
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-28 23:07:34 +0500

    Add header section

--- Projects/pricing-ui/app/components/pricingUi.tsx
+++ Projects/pricing-ui/app/components/pricingUi.tsx
@@ -15,10 +15,16 @@ import {
   List,
   ListItem,
   ListIcon,
+  Container,
 } from "@chakra-ui/react";
 
 export default function PricingUi() {
   return (
-    <>This is pricingUi </>
+    <>
+    <Box bg={'#6B46C1'} textAlign='center' color={'#F7FAFC'} h={'397px'}>
+      <Heading fontSize={'48px'} fontWeight={'800'} pt={'90px'}>Simple pricing for your business</Heading>
+      <Text fontSize={'24px'} fontWeight={'500'}>Plans that are carefully crafted to suit your business.</Text>
+    </Box>
+    </>
   );
 }

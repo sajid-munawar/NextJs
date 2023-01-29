1669eafe8a5dacec262c10a85b06a6412d742ac2
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-29 21:16:34 +0500

    Add styles

--- Projects/todo-app/app/page.tsx
+++ Projects/todo-app/app/page.tsx
@@ -3,6 +3,8 @@ import { DeleteIcon } from "@chakra-ui/icons";
 import {
   Button,
   ChakraProvider,
+  Checkbox,
+  Container,
   HStack,
   Input,
 } from "@chakra-ui/react";
@@ -13,12 +15,12 @@ export default function Page() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([
     {
-      todoText: "Abc",
+      todoText: "UI Design",
       completed: false,
     },
     {
-      todoText: "gef",
-      completed: true,
+      todoText: "Q1 tasks",
+      completed: false,
     },
   ]);
 
@@ -32,11 +34,13 @@ export default function Page() {
     setTodos(newTodo);
   };
 
-  const InputHandler = () => {
-    let newTodo = { todoText: todo, completed: false };
-    let newTodos = [newTodo, ...todos];
-    setTodos(newTodos);
-    setTodo("");
+  const addTodoHandler = () => {
+    if (todo) {
+      let newTodo = { todoText: todo, completed: false };
+      let newTodos = [newTodo, ...todos];
+      setTodos(newTodos);
+      setTodo("");
+    }
   };
 
   const deleteHandler = (currTodo: any) => {
@@ -48,41 +52,81 @@ export default function Page() {
   return (
     <>
       <ChakraProvider>
-        <Heading as="h1" style={{ textAlign: "center" }} color="red" pt="1em">
-          Todo App
-        </Heading>
-        <Input
-          type="text"
-          value={todo}
-          onChange={(e: any) => {
-            setTodo(e.target.value);
-          }}
-        />
-        <Button colorScheme="purple" type="submit" onClick={InputHandler}>
-          Add
-        </Button>
-        <Box maxWidth="500px" bg="gray" m="auto">
-          {todos.map((todo: any) => {
-            return (
-              <>
-                <HStack spacing="10px" key={todo.todoText}>
-                  <input
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={() => {
-                      checkedHandler(todo);
-                    }}
-                  />
-                  <Text>{todo.todoText}</Text>
-                  <DeleteIcon
-                    onClick={() => {
-                      deleteHandler(todo);
-                    }}
-                  />
-                </HStack>
-              </>
-            );
-          })}
+        <Box
+          w="100&"
+          height="100vh"
+          bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"
+        >
+          <Container>
+            <Heading
+              as="h1"
+              bgGradient="linear(to-l, #7928CA, #FF0080)"
+              bgClip="text"
+              fontSize="5xl"
+              fontWeight="extrabold"
+              py="3"
+              textAlign="center"
+            >
+              Todo APP
+            </Heading>
+            <Input
+              type="text"
+              size="md"
+              placeholder="Write todo here"
+              color="teal"
+              _placeholder={{ color: "inherit" }}
+              value={todo}
+              onChange={(e: any) => {
+                setTodo(e.target.value);
+              }}
+              onKeyDown={(e) => e.key === "Enter" && addTodoHandler()}
+            />
+            <Button
+              p={4}
+              my="2"
+              width="100%"
+              color="white"
+              fontWeight="bold"
+              borderRadius="md"
+              bgGradient="linear(to-r, teal.500, green.500)"
+              _hover={{
+                bgGradient: "linear(to-r, red.500, yellow.500)",
+              }}
+              type="submit"
+              onClick={addTodoHandler}
+            >
+              Add
+            </Button>
+            <Box maxWidth="500px" m="auto">
+              {todos.map((todo: any) => {
+                return (
+                  <>
+                    <HStack spacing="10px" key={todo.todoText} mb="2">
+                    <Checkbox colorScheme='green'
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => {
+                          checkedHandler(todo);
+                        }}
+                      />
+                      <Text width="90%" 
+                      style={{
+                        textDecoration:
+                        todo.completed? 'line-through':'none'
+                      }}
+                      >{todo.todoText}</Text>
+                      <DeleteIcon
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                          deleteHandler(todo);
+                        }}
+                      />
+                    </HStack>
+                  </>
+                );
+              })}
+            </Box>
+          </Container>
         </Box>
       </ChakraProvider>
     </>

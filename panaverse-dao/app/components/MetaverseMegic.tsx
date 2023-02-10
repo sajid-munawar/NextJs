ef448d7df76cbc221f79ad26d61628a619377852
sajid-munawar <sajidmunawar666@gmail.com>
2023-02-11 01:36:30 +0500

    Metaverse Magic for mobile

--- panaverse-dao/app/components/MetaverseMegic.tsx
+++ panaverse-dao/app/components/MetaverseMegic.tsx
@@ -6,17 +6,27 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const MetaverseMegic = () => {
   return (
-    <Flex justifyContent={['flex-start','flex-start',"space-around" ]} alignItems="center" flexDir={['column','column','row']} py={['0','0','50px']}>
-      <Box maxWidth={['100%','100%',"50%"]} >
+    <Flex
+      justifyContent={["flex-start", "flex-start", "space-around"]}
+      alignItems="center"
+      flexDir={["column", "column", "row"]}
+      py={["0", "0", "50px"]}
+    >
+      <Box maxWidth={["100%", "100%", "50%"]}>
         <Image
           src={metaverseMegic}
           alt="getting ready"
           className="gettingReady"
         />
       </Box>
-      <Box maxWidth={['100%','100%',"40%"]} py="10px" px='30px' textAlign="center">
+      <Box
+        maxWidth={["100%", "100%", "40%"]}
+        py="10px"
+        px="50px"
+        textAlign="center"
+      >
         <Heading>Metaverse Will Change Your Life Forever</Heading>
-        <Box textAlign="start">
+        <Box textAlign="start" px={["20px", "0", "0"]}>
           <Text as="i" fontSize="2xl" pt="20px">
             "We've gone from desktop to web to mobile; from text to photo to
             video. But that's not the end. The next platform will be even more
@@ -26,7 +36,7 @@ export const MetaverseMegic = () => {
           </Text>
           <br />
         </Box>
-        <Box textAlign="right">
+        <Box textAlign="right" pb={["100px", "100px", "0"]}>
           <Text as="b" fontSize="2xl">
             Mark Zuckerberg
           </Text>

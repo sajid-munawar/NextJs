882b5986d343607b7d0fbe5542e204a13e2015ac
sajid-munawar <sajidmunawar666@gmail.com>
2023-02-11 10:52:21 +0500

    Getting ready to faculty section responsive

--- panaverse-dao/app/components/GettingReady.tsx
+++ panaverse-dao/app/components/GettingReady.tsx
@@ -1,29 +1,50 @@
 "use client";
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import gettingReady from "../assets/gettingReady.jpg";
-import { Box,Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
+// <Flex justifyContent={['flex-start','flex-start',"space-around" ]}alignItems="center" flexDir={['column','column','row']}>
+
+{
+  /* <Box maxWidth={['100%','100%',"50%"]} mt={['-50px','-20px','0']}>
+  <Image
+      src={gettingReady}
+      alt="getting ready"
+      className="gettingReady"
+      />
+    </Box> */
+}
+// </Flex>
 export const GettingReady = () => {
   return (
-    <Flex justifyContent={['flex-start','flex-start',"space-around" ]}alignItems="center" flexDir={['column','column','row']}>
-      <Box maxWidth={['100%','100%',"50%"]} mt={['-50px','-20px','0']}>
+    <Grid templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)']} gap={2} px='2em'>
+      <GridItem>
         <Image
-          src={gettingReady}
+          src="./gettingReady.jpg"
+          objectFit="cover"
           alt="getting ready"
-          className="gettingReady"
+          borderRadius='20px'
+          mt='20px'
         />
-      </Box>
-      <Box maxWidth={['100%','100%',"40%"]} py="10px" px='20px' textAlign="center">
-        <Heading >
-          Getting Ready for the Next Generation of the Internet
-        </Heading>
-        <Text fontSize='2xl' pt='20px' >
-          Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI), Cloud,
-          Edge, Ambient Computing/IoT, Network Automation, and Bioinformatics
-          Technologies{" "}
-        </Text>
-      </Box>
-    </Flex>
+      </GridItem>
+      <GridItem>
+        <Box
+          py={["10px",'15px','100px']}
+          px="10px"
+          textAlign={['start','start',"center"]}
+        >
+          <Heading fontSize={['xl','2xl','4xl']}>
+            Getting Ready for the Next Generation of the Internet
+          </Heading>
+          <Text fontSize={['md','xl',"2xl"]} pt={['10px','15px',"20px"]} color='#718096'>
+            Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI),
+            Cloud, Edge, Ambient Computing/IoT, Network Automation, and
+            Bioinformatics Technologies{" "}
+          </Text>
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };

f3ae12db221adaed8aaada37759cbbd3b6e3ab16
sajid-munawar <sajidmunawar666@gmail.com>
2023-03-24 15:11:57 +0500

    Card completed for desktop

--- pricing-ui-tailwind/app/components/Card.tsx
+++ pricing-ui-tailwind/app/components/Card.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Tickmark from "../Assets/Tickmark";
 
 export default function Card() {
   return (
-    <div className="flex justify-center pt-20">
-        {/* left Box */}
-        <div>
-            <div>Premium PRO</div>
-            <div>$329</div>
-            <div>billed just once</div>
-            <button>Get Started</button>
-        </div>
-        {/* right Box */}
-        <div></div>
+    <div className="flex justify-center pt-20 text-black ">
+      {/* left Box */}
+      <div className="bg-gray-300 p-12 rounded-l-lg shadow-lg">
+        <div className="font-bold text-2xl">Premium PRO</div>
+        <span className="font-bold text-6xl">$329</span>
+        <p>billed just once</p>
+        <button
+          type="submit"
+          className="bg-violet-500 text-white font-medium py-3 px-16 rounded-md mt-6"
+        >
+          Get Started
+        </button>
+      </div>
+      {/* right Box */}
+      <div className="bg-white rounded-r-lg p-12 shadow-lg">
+        <p>
+          Access these features when you get this pricing package for your
+          business.
+        </p>
+        <ul>
+          <li>
+            <Tickmark />
+            <p>International calling and messaging API</p>
+          </li>
+          <li>
+            <Tickmark />
+            <p>Additional phone numbers</p>
+          </li>
+          <li>
+            <Tickmark />
+            <p>Automated messages via Zapier</p>
+          </li>
+          <li>
+            <Image src={"/tick.png"} alt="tick" width={22} height={22} />
+            <p>24/7 support and consulting</p>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }

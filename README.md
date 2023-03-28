04df3a97cb04fb77649322c646fa457d9583a586
sajid-munawar <sajidmunawar666@gmail.com>
2023-03-28 10:21:44 +0500

    Add pricing ui tailwind links

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Pricing UI using ChakraUi and NextJs
 [See Live](https://pricing-ui-kohl.vercel.app/)  
 [Source Code](https://github.com/sajid-munawar/NextJs/tree/main/pricing-ui)
+
+## Pricing UI using Tailwind and NextJs
+[See Live](https://pricing-ui-tailwind-lake.vercel.app/)  
+[Source Code](https://github.com/sajid-munawar/NextJs/tree/main/pricing-ui-tailwind)
--- resume-website/app/components/Navbar.tsx
+++ resume-website/app/components/Navbar.tsx
@@ -4,14 +4,14 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Navbar = () => {
   return (
-    <div className='flex p-4 bg-slate-600 text-white justify-center items-center'>
+    <div className='flex p-4 text-white justify-center items-center'>
         <h1 className='font-MyName text-2xl'>Sajid Munawar</h1>
         <div className='hidden  ml-auto md:flex gap-5 font-cursive font-medium'>
-            <Link href="/" className='md:hover:scale-125 duration-500'>Home</Link>
-            <Link href="/" className='md:hover:scale-125 duration-500'>Linkbout</Link>
-            <Link href="/" className='md:hover:scale-125 duration-500'>Projects</Link>
-            <Link href="/" className='md:hover:scale-125 duration-500'>Skills</Link>
-            <Link href="/" className='md:hover:scale-125 duration-500'>Contact</Link>
+            <Link href="/" className='navbar-links'>Home</Link>
+            <Link href="/" className='navbar-links'>Linkbout</Link>
+            <Link href="/" className='navbar-links'>Projects</Link>
+            <Link href="/" className='navbar-links'>Skills</Link>
+            <Link href="/" className='navbar-links'>Contact</Link>
         </div>        
         <RxHamburgerMenu className='md:hidden ml-auto'/>
     </div>

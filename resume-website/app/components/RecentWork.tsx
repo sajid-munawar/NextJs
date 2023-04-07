fe2f51c3bb12fe02cab2fc407768b8edfe8914f4
sajid-munawar <sajidmunawar666@gmail.com>
2023-04-07 11:17:32 +0500

    My Recent works start

---
+++ resume-website/app/components/RecentWork.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RecentWork = () => {
+  return (
+    <div className='px-6'>
+        <h1 className='font-crete text-4xl text-gray-800 pt-32'>My Recent Works</h1>
+        <hr />
+    </div>
+  )
+}
+
+export default RecentWork
--- resume-website/app/page.tsx
+++ resume-website/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import RecentWork from "./components/RecentWork";
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <Hero/>
+    <RecentWork />
     </>
   )
 }

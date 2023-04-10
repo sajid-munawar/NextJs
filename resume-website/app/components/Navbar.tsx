e8c480426efa0fb99de68ff736f42f1ecc72e8db
sajid-munawar <sajidmunawar666@gmail.com>
2023-04-10 10:48:22 +0500

    Complete navbar for desktop

--- resume-website/app/components/Navbar.tsx
+++ resume-website/app/components/Navbar.tsx
@@ -11,8 +11,7 @@ const Navbar = () => {
   };
   const MobilePopUp = () => {
     return (
-      <div className="font-inter text-4xl h-screen w-full fixed top-0 left-0 font-bold bg-gray-700 text-white">
-        {/* close button was here */}
+      <div className="font-inter text-4xl h-screen w-full fixed top-0 left-0 font-bold bg-gray-700 text-white md:hidden">
         <ul className="px-6 pt-20">
           <li>
             <a href="#portfolio" onClick={handleClicked}>
@@ -38,25 +37,30 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex p-4 text-white justify-center items-center bg-gray-700">
-      <h1 className="font-roboto text-lg">Sajid Munawar</h1>
-      <div className="hidden  ml-auto md:flex gap-5 font-cursive font-medium">
-        <Link href="/" className="navbar-links">
-          Home
-        </Link>
-        <Link href="/" className="navbar-links">
-          Linkbout
-        </Link>
-        <Link href="/" className="navbar-links">
-          Projects
-        </Link>
-        <Link href="/" className="navbar-links">
-          Skills
-        </Link>
-        <Link href="/" className="navbar-links">
-          Contact
-        </Link>
+    <div className="flex p-4 text-white justify-center items-center bg-gray-700 w-full">
+      <h1 className="font-roboto text-lg md:ml-36 md:text-xl">Sajid Munawar</h1>
+      {/* Desktop links starts here */}
+
+      <div className="hidden md:flex ml-auto mr-10">
+      <ul className="flex gap-10">
+          <li>
+            <a href="#portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
+      {/* Desktop links ends here */}
       <div className="md:hidden ml-auto z-20" onClick={handleClicked}>
         {menuOpen ? <CloseButton /> : <Hamburger />}
       </div>

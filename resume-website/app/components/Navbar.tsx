4c31ad0c2952c39d615f7f60745944e69f8215db
sajid-munawar <sajidmunawar666@gmail.com>
2023-04-10 10:26:00 +0500

    Update logic of navbar

--- resume-website/app/components/Navbar.tsx
+++ resume-website/app/components/Navbar.tsx
@@ -5,23 +5,33 @@ import Hamburger from "../Icons/Hamburger";
 import CloseButton from "../Icons/CloseButton";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
   const handleClicked = () => {
     setMenuOpen(!menuOpen);
   };
   const MobilePopUp = () => {
     return (
-      <div className="font-inter text-4xl h-screen w-full fixed top-0 left-0 bottom-0 font-bold bg-gray-700 text-white">
-        <div className="fixed right-5 top-5" onClick={handleClicked}>
-            <CloseButton />{" "}
-          </div>
+      <div className="font-inter text-4xl h-screen w-full fixed top-0 left-0 font-bold bg-gray-700 text-white">
+        {/* close button was here */}
         <ul className="px-6 pt-20">
-        <li><a href="#portfolio" onClick={handleClicked}>Portfolio</a></li>
-          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>  
-          <li><a href="#about" onClick={handleClicked}>About</a></li>
-          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>  
-          <li><a href="#contact" onClick={handleClicked}>Contact</a></li>
-          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>  
+          <li>
+            <a href="#portfolio" onClick={handleClicked}>
+              Portfolio
+            </a>
+          </li>
+          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>
+          <li>
+            <a href="#about" onClick={handleClicked}>
+              About
+            </a>
+          </li>
+          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>
+          <li>
+            <a href="#contact" onClick={handleClicked}>
+              Contact
+            </a>
+          </li>
+          <div className="h-[1px] bg-gray-500 mb-20 mt-3"></div>
         </ul>
       </div>
     );
@@ -47,18 +57,10 @@ const Navbar = () => {
           Contact
         </Link>
       </div>
-      <div className="md:hidden ml-auto">
-        {!menuOpen && (
-          <div onClick={handleClicked}>
-            <Hamburger />{" "}
-          </div>
-        )}
-        {menuOpen && (
-          <div>
-            <MobilePopUp />{" "}
-          </div>
-        )}
+      <div className="md:hidden ml-auto z-20" onClick={handleClicked}>
+        {menuOpen ? <CloseButton /> : <Hamburger />}
       </div>
+      {menuOpen && <MobilePopUp />}
     </div>
   );
 };

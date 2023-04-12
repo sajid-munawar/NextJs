6eb74aadf087b2286e283313026aa0cbb69fba94
sajid-munawar <sajidmunawar666@gmail.com>
2023-04-12 10:46:21 +0500

    Add links to footer social icons

--- resume-website/app/components/Contact.tsx
+++ resume-website/app/components/Contact.tsx
@@ -4,30 +4,35 @@ const Contact = () => {
   return (
     <div className="px-6 py-20 lg:flex gap-10  lg:py-36 lg:px-20" id="contact">
       <div className="flex justify-center">
-      <h1 className="text-cyan-900 text-3xl lg:text-4xl lg:text-start font-bold font-inter text-center w-[327px] lg:w-[400px] lg:tracking-wider">
-        I&apos;m always <br className="lg:hidden" /> interested in hearing about new projects, so if you&apos;d like to
-        chat please get in touch.
-      </h1>
+        <h1 className="text-cyan-900 text-3xl lg:text-4xl lg:text-start font-bold font-inter text-center w-[327px] lg:w-[400px] lg:tracking-wider">
+          I&apos;m always <br className="lg:hidden" /> interested in hearing
+          about new projects, so if you&apos;d like to chat please get in touch.
+        </h1>
       </div>
-      <form id="form" action="https://formspree.io/f/myyodjdo" method="post" className="flex flex-col">
-          <input
-            type="text"
-            placeholder="Full name"
-            name="Full name"
-            id="fName"
-            maxLength={30}
-            required
-            className="mt-40 lg:mt-0 pl-3 border-none text-xl h-10"
-          />
-          <input
-            type="email"
-            placeholder="Email address"
-            name="email"
-            id="email"
-            required
-            className="my-10 pl-3 border-none text-xl h-10"
-          />
-          <textarea
+      <form
+        id="form"
+        action="https://formspree.io/f/myyodjdo"
+        method="post"
+        className="flex flex-col"
+      >
+        <input
+          type="text"
+          placeholder="Full name"
+          name="Full name"
+          id="fName"
+          maxLength={30}
+          required
+          className="mt-40 lg:mt-0 pl-3 border-none text-xl h-10"
+        />
+        <input
+          type="email"
+          placeholder="Email address"
+          name="email"
+          id="email"
+          required
+          className="my-10 pl-3 border-none text-xl h-10"
+        />
+        <textarea
           cols={30}
           rows={7}
           placeholder="Write me something..."
@@ -36,14 +41,13 @@ const Contact = () => {
           id="message"
           name="Message"
           className="my-10 pl-3 border-none text-xl bg-[url('/formbg.png')] bg-no-repeat bg-right-bottom"
-          ></textarea>
-          <div className="text-center">
-
+        ></textarea>
+        <div className="text-center lg:text-start">
           <button className="font-inter font-bold bg-orange-500 p-3 tracking-wider text-lg text-white">
-          Get in touch
-        </button>
-          </div>
-        </form>
+            Get in touch
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- resume-website/app/components/SocialIcons.tsx
+++ resume-website/app/components/SocialIcons.tsx
@@ -9,15 +9,25 @@ import Shape from "../Icons/Shape";
 const SocialIcons = () => {
   return (
     <div>
-      <div className="h-[1px] bg-gray-500 mt-20"></div>
-      <div className="flex justify-center mt-3 mb-5">
-        <GitHubB />
-        <LinkedInB />
-        <AngleB />
-        <TwitterB />
-        <MediumB />
+      <div className="h-[1px] bg-gray-500 mt-20 lg:hidden"></div>
+      <div className="flex justify-center mt-3 mb-5 lg:mb-0 lg:py-10 lg:gap-6">
+        <a href="https://github.com/sajid-munawar" target="_blank">
+          <GitHubB />
+        </a>
+        <a href="https://www.linkedin.com/in/sajid-munawar" target="_blank">
+          <LinkedInB />
+        </a>
+        <a href="https://wellfound.com/u/sajid-munawar-1" target="_blank">
+          <AngleB />
+        </a>
+        <a href="https://twitter.com/SajidMunawarDev" target="_blank">
+          <TwitterB />
+        </a>
+        <a href="https://medium.com/@iamsajiddev" target="_blank">
+          <MediumB />
+        </a>
       </div>
-      <div className="flex justify-center py-5">
+      <div className="flex justify-center py-5 lg:hidden ">
         <Shape />
       </div>
     </div>

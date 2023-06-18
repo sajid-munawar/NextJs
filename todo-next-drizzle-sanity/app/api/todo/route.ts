065eea47fa01eaee532a1fecb8f35eadaa416b6e
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-18 14:08:15 +0500

    Created get and post api's using drizzle

--- todo-next-drizzle-sanity/app/api/todo/route.ts
+++ todo-next-drizzle-sanity/app/api/todo/route.ts
@@ -1,32 +1,30 @@
-import {db} from '@vercel/postgres'
+// import {db} from '@vercel/postgres'
 import {NextRequest, NextResponse} from 'next/server' 
+import {db,todoTable,Todo,NewTodo} from '@/app/lib/drizzle';
+import { sql } from '@vercel/postgres';
 
 export async function GET(request: NextRequest){
-    const client=db.connect()
-
+    
     try {
-        (await client).sql`CREATE TABLE Todos(id serial, Task varchar(255))`        
+        await sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255))`
+        const res=await db.select().from(todoTable)
+        console.log(res);
+        return NextResponse.json({data:res})
     } catch (error) {
         console.log('error :>> ', error);
-        return new Response('Something went wrong')
-        
+        return NextResponse.json({message:'Something went wrong'})
     }
-
-    return NextResponse.json({message:'You called an api'})
 }
 
 
 export const POST = async (request:NextRequest)=>{
-    const req= await request.json()
-    const client=await db.connect()
-    try {
-        if(req.task){
-            await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})` 
-            return NextResponse.json({message:'Data saved successfuly'})
-        }else {
-            throw new Error("Task field is required")
-        }
-    } catch (error) {
-        return NextResponse.json({message: (error as {message:string}).message})        
-    }
+    const body= await request.json()
+    const data = await db.insert(todoTable).values(body).returning();
+
+  return new NextResponse(
+    JSON.stringify({
+      message: "Data Added",
+      data,
+    })
+  );
 }

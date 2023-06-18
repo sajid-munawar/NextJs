29f9197810ca044bfde9b7bf90182997d300bba5
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-18 10:17:50 +0500

    Doing post request for todos successfully

--- todo-next-drizzle-sanity/app/api/todo/route.ts
+++ todo-next-drizzle-sanity/app/api/todo/route.ts
@@ -1,11 +1,11 @@
 import {db} from '@vercel/postgres'
 import {NextRequest, NextResponse} from 'next/server' 
 
-export function GET(request: NextRequest){
+export async function GET(request: NextRequest){
     const client=db.connect()
 
     try {
-        
+        (await client).sql`CREATE TABLE Todos(id serial, Task varchar(255))`        
     } catch (error) {
         console.log('error :>> ', error);
         return new Response('Something went wrong')
@@ -13,4 +13,20 @@ export function GET(request: NextRequest){
     }
 
     return NextResponse.json({message:'You called an api'})
+}
+
+
+export const POST = async (request:NextRequest)=>{
+    const req= await request.json()
+    const client=await db.connect()
+    try {
+        if(req.task){
+            await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})` 
+            return NextResponse.json({message:'Data saved successfuly'})
+        }else {
+            throw new Error("Task field is required")
+        }
+    } catch (error) {
+        return NextResponse.json({message: (error as {message:string}).message})        
+    }
 }

a349cfb7f2c0ed0068b9f512ed5ac82454f61ee0
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-21 14:29:58 +0500

    Update route.ts

--- todo-next-drizzle-sanity/app/api/todo/route.ts
+++ todo-next-drizzle-sanity/app/api/todo/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db, todoTable } from "@/app/lib/drizzle";
-// import { sql } from "@vercel/postgres";
 import { and, eq } from "drizzle-orm";
 import { cookies } from "next/headers";
 import { v4 as uuid } from "uuid";
 
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.searchParams.get("id");
-  console.log('iddddd', id)
   if(id){
     try {
         const res = await db.select().from(todoTable).where(eq(todoTable.user_id,id))
@@ -20,7 +18,6 @@ export async function GET(request: NextRequest) {
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
-  console.log('body', body)
   const user_id = cookies().get("user_id")?.value;
   if (!user_id) {
     const uid = uuid();
@@ -46,7 +43,6 @@ export async function DELETE(request: NextRequest) {
     await db.delete(todoTable).where(and(eq(todoTable.id, id),eq(todoTable.user_id,user_id))).execute();
     return NextResponse.json({ message: "Todo deleted successfully" });
   } catch (error) {
-    console.log("error :>> ", error);
     return NextResponse.json({ message: "Something went wrong" });
   }
 }

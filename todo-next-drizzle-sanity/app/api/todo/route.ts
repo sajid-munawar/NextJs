ea6514ea9242a505b5207d68c7ce7633952e50d4
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-21 13:48:19 +0500

    Update route.ts
    Get and Add task is working as expected

--- todo-next-drizzle-sanity/app/api/todo/route.ts
+++ todo-next-drizzle-sanity/app/api/todo/route.ts
@@ -1,38 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db, todoTable } from "@/app/lib/drizzle";
-import { sql } from "@vercel/postgres";
+// import { sql } from "@vercel/postgres";
 import { eq } from "drizzle-orm";
 import { cookies } from "next/headers";
 import { v4 as uuid } from "uuid";
 
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.searchParams.get("id");
-  console.log("id :>> ", id);
-  // const uid = cookies().get('user_id')?.value
-  // const uid =cookies().get('user_id')?.value
-  // console.log('uid', uid)
-  // if(uid){
-  // }else{
-  //   console.log('not found')
-  // }
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255))`;
-    const res = await db.select().from(todoTable);
-    return NextResponse.json(res);
-  } catch (error) {
-    return NextResponse.json({ message: "Something went wrong" });
-  }
+  console.log('iddddd', id)
+  if(id){
+    try {
+        const res = await db.select().from(todoTable).where(eq(todoTable.user_id,id))
+        return NextResponse.json(res);
+      } catch (error) {
+        return NextResponse.json({ message: "Something went wrong" });
+      }
+  }  
 }
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
+  console.log('body', body)
   const user_id = cookies().get("user_id")?.value;
-  const uid = uuid();
-
   if (!user_id) {
+    const uid = uuid();
     cookies().set("user_id", uid);
   }
-  const data = await db.insert(todoTable).values(body).returning();
+  const data = await db.insert(todoTable).values({
+    task:body.task,
+    user_id:cookies().get("user_id")?.value as string
+  }).returning();
 
   return new NextResponse(
     JSON.stringify({

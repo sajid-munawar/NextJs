faae8ec4acf867336937d415e5b6c1ee9bb0fc69
Sajid Munawar <iamsajiddev@gmail.com>
2023-08-01 11:34:24 +0500

    Api is returning data perfectly in server component

--- nextjs-auth/src/app/page.tsx
+++ nextjs-auth/src/app/page.tsx
@@ -1,17 +1,41 @@
-import Link from "next/link";
+// "use client";
 
-export default function Home() {
+// import { useEffect } from "react";
+
+const getMatches = async () => {
+  const response = await fetch(
+    "http://api.football-data.org/v4/teams/67/matches/",
+    {
+      mode: "no-cors",
+      headers: {
+        "content-type": "application/json",
+        "X-Auth-Token": "41159774f9884bb38d002ac08f5c3719",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log("data", data);
+  return data.matches;
+};
+export default async function Home() {
+  // useEffect(() => {
+  const data = await getMatches();
+  //   console.log("data1", data);
+  // }, []);
+
   return (
-    <section className="bg-black text-white rounded-xl p-10 w-4/5 flex flex-col justify-center items-center gap-10 mx-auto grow mb-10 drop-shadow-lg text-center font-bold">
-      <h1 className="text-6xl">Let's start authenicating with Kinde</h1>
-      <p className="text-4xl">Configure your app</p>
-      <Link
-        href="https://kinde.com/docs/sdks/nextjs-sdk"
-        target="_blank"
-        className="text-2xl bg-gray-100 text-black px-4 py-2 rounded"
-      >
-        Go to Docs
-      </Link>
+    <section>
+      <h1>Matches of Newcastle United FC</h1>
+      <ul>
+        {data &&
+          data?.map((match: any) => (
+            <li key={match.id}>
+              Match ID: {match.id}
+              Date: {new Date(match.utcDate).toLocaleString()}
+              Status: {match.status}
+            </li>
+          ))}
+      </ul>
     </section>
   );
 }

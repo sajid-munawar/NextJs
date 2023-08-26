daaf8e8d34d77418d6c5b06de9790d5ee2cb1e92
Sajid Munawar <iamsajiddev@gmail.com>
2023-08-26 11:58:37 +0500

    Create route.ts

---
+++ jwt-practice/src/app/api/login/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
+import { cookies } from "next/headers";
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json().catch(() => null);
+
+  if (body.email === "admin" && body.password === "admin") {
+    const secret = new TextEncoder().encode(process.env.SECRET_KEY);
+
+    console.log("secret: ", secret);
+    const alg = "HS256";
+
+    const jwt = await new jose.SignJWT({ email: body.email })
+      .setProtectedHeader({ alg })
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .sign(secret);
+    console.log("jwt", jwt);
+
+    cookies().set("token", jwt, {
+      httpOnly: true,
+    });
+
+    return NextResponse.json({ message: "Login successss" });
+  }
+
+  return NextResponse.json({ message: "Invalid Email or password" });
+};

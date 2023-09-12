12f4ed40623e2efb3b80da5abeb6a4f0618d5cbd
Sajid Munawar <iamsajiddev@gmail.com>
2023-09-12 19:41:44 +0500

    Added css modules in about page

--- With Dave Gray/nextjs-with-dave-gray/src/app/about/page.tsx
+++ With Dave Gray/nextjs-with-dave-gray/src/app/about/page.tsx
@@ -1,13 +1,16 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css";
 
 const page = () => {
   return (
     <>
-    <h1>About page</h1>
-    <Link href='/'>Go to home</Link>
+      <main className={styles.main}>
+        <h1>About page</h1>
+        <Link href="/">Go to home</Link>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
